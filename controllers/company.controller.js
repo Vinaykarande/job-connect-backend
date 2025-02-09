2f7481e827d8ae7f11c7ff64e08ad1a232d6a530
Vinay Karande <vinaykarande021@gmail.com>
2025-02-09 18:20:21 +0530

    Added Job Controller

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -59,7 +59,7 @@ export const getCompany = async (req,res) =>{
 export const getCompanyById = async(req,res) =>{
     try {
         const companyId = req.params.id;
-        const company = Company.findById(companyId);
+        const company = await Company.findById(companyId);
 
         if(!company){
             return res.status(404).json({
